3bb41fefe6a64ea64fe1ae374b6a0781a29c9a04
Mangai <mvelusamy@healthplan.com>
2018-12-27 14:40:16 +0530

    First Git commit

---
+++ src/GitDemo.java
@@ -0,0 +1,14 @@
+
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String favocolor = "Blue and Pink";
+		System.out.print("My Favo color is : ");
+		System.out.print(favocolor);
+	/*System.out.print(favofood); */
+	}
+
+}
+
